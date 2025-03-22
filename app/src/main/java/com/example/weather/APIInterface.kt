eb1e3734b33ef18cc51a994859c5debca7376651
HeetChaniyara <heetchaniyara19@gmail.com>
2025-03-22 09:48:08 +0530

    Models class added UI Change And Updates

---
+++ app/src/main/java/com/example/weather/APIInterface.kt
@@ -0,0 +1,17 @@
+package com.example.weather
+
+import com.example.weather.models.WeatherData
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface APIInterface {
+
+
+    @GET("weather")
+    fun getWeatherData(
+        @Query("q") city:String,
+        @Query("appid") appid :String,
+        @Query("units") units:String
+    ) : Call<WeatherData>
+}
--- app/src/main/java/com/example/weather/MainActivity.kt
+++ app/src/main/java/com/example/weather/MainActivity.kt
@@ -1,17 +1,33 @@
 package com.example.weather
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.weather.databinding.ActivityMainBinding
+import com.example.weather.models.WeatherData
+import org.w3c.dom.Text
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
+
+//    47561fc40c887a4bbb468d6530937de1
 
     private val binding: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +36,36 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.toolbarDetail)
 
+        fetchWeatherData()
+
     }
+
+    private fun fetchWeatherData() {
+        val retrofit = Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl("https://api.openweathermap.org/data/2.5/")
+            .build()
+            .create(APIInterface::class.java)
+
+        val response = retrofit.getWeatherData("vapi", "47561fc40c887a4bbb468d6530937de1", "metric")
+        response.enqueue(object : Callback<WeatherData>{
+            override fun onResponse(call: Call<WeatherData>, response: Response<WeatherData>) {
+                val responseBody = response.body()
+                if (response.isSuccessful && responseBody != null) {
+                    val temperature = responseBody.main.temp.toString()
+
+                    binding.mainTempDegree.text = "${temperature} \u00B0C"
+                }
+            }
+
+            override fun onFailure(call: Call<WeatherData>, response: Throwable) {
+                TODO("Not yet implemented")
+            }
+
+        })
+
+    }
+
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.menu, menu)
